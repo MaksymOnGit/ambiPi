93d4c5e4a13857cbdf58d89f9aa93fd9098efce4
Maksym <maksym.yezhov@gmail.com>
2020-11-01 19:33:34 +0000

    New, faster algorithm

--- ambiPi.py
+++ ambiPi.py
@@ -5,17 +5,22 @@ from picamera.color import Color
 import time
 
 INPUT_RES = (1920, 1088)
-PROC_RES = (640, 360)
+PROC_RES = (64, 48)#(640,360)
 LED_RES = (37, 21)
         
 
 class FrameProcessor(PiRGBAnalysis):
     def __init__(self, camera, resolution):
         super(FrameProcessor, self).__init__(camera, size=resolution)
-        self.fullW = PROC_RES[0] if PROC_RES else INPUT_RES[0]
-        self.fullH = PROC_RES[1] if PROC_RES else INPUT_RES[1]
-        self.blockW = self.fullW//LED_RES[0]
-        self.blockH = self.fullH//LED_RES[1]
+
+        self.ledW = LED_RES[0]
+        self.ledH = LED_RES[1]
+
+        self.procW = PROC_RES[0]
+        self.procH = PROC_RES[1]
+
+        self.blockW = self.procW / self.ledW
+        self.blockH = self.procH / self.ledH
 
         print(chr(27)+'[2j')
         print('\033c')
@@ -26,33 +31,44 @@ class FrameProcessor(PiRGBAnalysis):
         self.lastreport = time.time()
 
     def analyze(self, data):
-        for i in range(self.blockW, self.fullW, self.blockW):
-            r=int(numpy.mean(data[0:self.blockH, i:i+self.blockW, 0]))
-            g=int(numpy.mean(data[0:self.blockH, i:i+self.blockW, 1]))
-            b=int(numpy.mean(data[0:self.blockH, i:i+self.blockW, 2]))
-            self.printAt(1, (i*2)//self.blockW, r, g, b, '██')
-                
-        for i in range(self.blockH, self.fullH-self.blockH, self.blockH):
-            r=int(numpy.mean(data[i:i+self.blockH, self.fullW-self.blockW:self.fullW, 0]))
-            g=int(numpy.mean(data[i:i+self.blockH, self.fullW-self.blockW:self.fullW, 1]))
-            b=int(numpy.mean(data[i:i+self.blockH, self.fullW-self.blockW:self.fullW, 2]))
-            self.printAt((i+self.blockH)//self.blockH, 74, r, g, b, '██')
-            
-        for i in range(self.fullW-self.blockW, 0, -self.blockW):
-            r=int(numpy.mean(data[self.fullH-self.blockH-1:self.fullH-1, i:i+self.blockW, 0]))
-            g=int(numpy.mean(data[self.fullH-self.blockH-1:self.fullH-1, i:i+self.blockW, 1]))
-            b=int(numpy.mean(data[self.fullH-self.blockH-1:self.fullH-1, i:i+self.blockW, 2]))
-            self.printAt(21, (i*2//self.blockW), r, g, b, '██')
-            
-        for i in range(self.fullH-self.blockH, 0, -self.blockH):
-            r=int(numpy.mean(data[i:i+self.blockH, 0:self.blockW, 0]))
-            g=int(numpy.mean(data[i:i+self.blockH, 0:self.blockW, 1]))
-            b=int(numpy.mean(data[i:i+self.blockH, 0:self.blockW, 2]))
-            self.printAt(i//self.blockH, 1, r, g, b, '██')
+        for i in range(1, self.ledW):
+            x = round(i * self.blockW)
+            y = 0
+            r=int(data[y, x, 0])
+            g=int(data[y, x, 1])
+            b=int(data[y, x, 2])
+            self.printLED(0, i, r, g, b)
+
+        for i in range(1, self.ledH):
+            x = self.procW-1
+            y = round(i * self.blockH)
+            r=int(data[y, x, 0])
+            g=int(data[y, x, 1])
+            b=int(data[y, x, 2])
+            self.printLED(i, self.ledW-1, r, g, b)
+
+        for i in range(self.ledW-2, -1, -1):
+            x = round(i * self.blockW)
+            y = self.procH-1
+            r=int(data[y, x, 0])
+            g=int(data[y, x, 1])
+            b=int(data[y, x, 2])
+            self.printLED(self.ledH-1, i, r, g, b)
+
+        for i in range(self.ledH-1, -1, -1):
+            x = 0
+            y = round(i * self.blockH)
+            r=int(data[y, x, 0])
+            g=int(data[y, x, 1])
+            b=int(data[y, x, 2])
+            self.printLED(i, 0, r, g, b)
         self.report()
 
     def printAt(self, y, x, r, g, b, t):
-        print(f'\033[{y};{x}H\033[38;2;{r};{g};{b}m{t}', end='')
+        print(f'\033[{y+1};{x+1}H\033[38;2;{r};{g};{b}m{t}', end='')
+
+    def printLED(self, y, x, r, g, b):
+        self.printAt(y, x*2, r, g, b, '██')
 
     def report(self):
         if (time.time()-self.lastreport)>1.0:
